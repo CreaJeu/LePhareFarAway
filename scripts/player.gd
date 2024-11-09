d8dc24534893d4a191976502f19d6375ef372cae
EnzoButtiero <94783966+EnzoButtiero@users.noreply.github.com>
2024-11-09 14:21:37 +0100

    backgroundtest

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,15 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 @export var is_limited := true
 
 func _process(delta):
 	if is_limited:
-		if self.position.x > 400:
-			self.position.x = -400
-		if self.position.x < -400:
-			self.position.x = 400
+		if self.position.x > 640:
+			self.position.x = -640
+		if self.position.x < -640:
+			self.position.x = 640
 
 func _physics_process(delta):
 	var direction := Input.get_vector("left", "right", "ui_up", "ui_down")
