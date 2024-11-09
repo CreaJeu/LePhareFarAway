ec0fc5f444cc6fe4afaf7ad10ee201958b5d716d
Yvan <yvandouis@hotmail.fr>
2024-11-09 18:26:40 +0100

    walk animation + idle + no more inertia

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const SPEED = 1000.0
 
 @export var is_limited := true
+@onready var  _animation_player := $WorldModel/AnimatedSprite2D
 
 func _process(delta):
 	if is_limited:
@@ -14,8 +15,23 @@ func _process(delta):
 func _physics_process(delta):
 	var direction := Input.get_vector("left", "right", "ui_up", "ui_down")
 	if direction:
-		velocity.x = lerp(velocity.x,direction.x * SPEED,delta)
+		#velocity.x = lerp(velocity.x,direction.x * SPEED,delta)
+		velocity.x = direction.x * SPEED
 	else:
 		velocity.x = lerp(velocity.x,move_toward(velocity.x, 0, SPEED),delta * 10)
+		
+	if Input.is_action_pressed("left"):
+		_animation_player.play("walk")
+		_animation_player.flip_h = true
+		
+		
+	elif Input.is_action_pressed("right"):
+		print_debug("action pressed right")
+		_animation_player.play("walk")
+		_animation_player.flip_h = false
+		
+	else:
+		_animation_player.play("default")
+		
 
 	move_and_slide()
