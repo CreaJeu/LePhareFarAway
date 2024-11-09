8ed7f3efa32f108fb24421640c02811fe3e05ecf
EnzoButtiero <94783966+EnzoButtiero@users.noreply.github.com>
2024-11-09 12:31:28 +0100

    temp

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,13 +3,23 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-func _physics_process(delta: float) -> void:
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+@export var is_limited := true
+
+func _process(delta):
+	if $Area2D.overlaps_area(""):
+		print("nice")
+	
+	if is_limited:
+		if self.position.x > 400:
+			self.position.x = -400
+		if self.position.x < -400:
+			self.position.x = 400
+
+func _physics_process(delta):
+	var direction := Input.get_vector("left", "right", "ui_up", "ui_down")
 	if direction:
 		velocity.x = lerp(velocity.x,direction.x * SPEED,delta)
-		velocity.y = lerp(velocity.y,direction.y * SPEED,delta)
 	else:
 		velocity.x = lerp(velocity.x,move_toward(velocity.x, 0, SPEED),delta * 10)
-		velocity.y = lerp(velocity.y,move_toward(velocity.y, 0, SPEED),delta * 10)
 
 	move_and_slide()
