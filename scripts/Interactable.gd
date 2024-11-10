2c0d9a5e35fa5132c6bcd316bde9067a28b94c0f
Stepan Tyurin <sanstepon5@gmail.com>
2024-11-10 12:28:45 +0100

    Change how interactable works

--- scripts/Interactable.gd
+++ scripts/Interactable.gd
@@ -1,20 +1,21 @@
 extends Node2D
 
+signal interactable_used(interaction_name: String)
+
 var playerInZone: bool = false
 
-@export var sizeX : float
-@export var sizeY : float
+@export var parent: Sprite2D
+@export var interaction_name: String = ""
 @onready var collision: CollisionShape2D = $Area2D/CollisionShape2D
 
 func _ready():
-	collision.shape.size.x = sizeX
-	collision.shape.size.y = sizeY
+	collision.shape.size = parent.texture.get_size() * parent.scale
 
 func _process(delta):
 	if Input.is_action_just_pressed("interact") and playerInZone:
-		# TODO: do item action
-		print("Do interactable action")
-		$"..".do_action()
+		# Interactable node emits the signal with its name. It's parent must capture this signal and do an action with it
+		# The name is probably not useful
+		interactable_used.emit(interaction_name)
 
 
 func _on_area_2d_body_entered(body):
--- scripts/stairway.gd
+++ scripts/stairway.gd
@@ -3,5 +3,8 @@ extends Node2D
 @export var player: CharacterBody2D
 @export var step = -750
 
-func do_action():
+func _ready() -> void:
+	$Sprite2D/Interactable.connect("interactable_used", do_action)
+
+func do_action(interaction_name: String):
 	player.position.y += step
