0388d19c1f62e876d5587d0d56e1028c769d8a00
ulejoyeu <krilius974@gmail.com>
2024-11-10 21:20:38 +0100

    pas manger caffard

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -1,8 +1,19 @@
 extends Node2D
 
-@export var food = preload("res://scenes/prefabs/food_bean.tscn")
+@export var bean = preload("res://scenes/prefabs/food_bean.tscn")
+@export var cockroach = preload("res://scenes/prefabs/food_cockroach.tscn")
 
 func _ready() -> void:
-	var foodd = food.instantiate()
+	pass
+
+
+func _on_timer_timeout():
+	var foodd: Node2D = null
+	var rand_nf = randi() % 100
+	if rand_nf < 50:
+		foodd = bean.instantiate()
+	else:
+		foodd = cockroach.instantiate()	
+	
 	add_child(foodd)
 	foodd.position.x = randf_range(0,1100)
--- scripts/tetemninijeu.gd
+++ scripts/tetemninijeu.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
 const SPEED = 700.0
+var nb_food_eaten: int = 0
+var max_food_eaten: int = 10
+
+@export var nb_beans_eaten_label: RichTextLabel
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("left", "right")
@@ -10,3 +14,24 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_mouth_area_2d_body_entered(body: Node2D):
+	if "type_" in body and body.type_ == "bean":
+		nb_food_eaten += 1
+		nb_beans_eaten_label.text = "%d / 10" % nb_food_eaten
+		body.queue_free()
+	elif "type_" in body and body.type_ == "cockroach":
+		nb_food_eaten -= 1
+		nb_beans_eaten_label.text = "%d / 10" % nb_food_eaten
+		body.queue_free()
+	
+
+
+func _on_fallen(body: Node2D):
+	if "type_" in body and body.type_ == "bean":
+		nb_food_eaten -= 1
+		nb_beans_eaten_label.text = "%d / 10" % nb_food_eaten
+		body.queue_free()
+	elif "type_" in body and body.type_ == "cockroach":
+		body.queue_free()
